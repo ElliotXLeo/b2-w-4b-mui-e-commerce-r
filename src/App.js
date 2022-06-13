07f15b7d119441cf07dbd11592aec0a9dfe1a54a
ElliotXLeo <elliotgaramendi@gmail.com>
2022-06-13 18:13:55 -0500

    component ProductsCard

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import ProductsCard from './components/products/ProductsCard';
 
 function App() {
   return (
-    <h1>Base 2 by Web 4B Bueno, Bonito, Barato y Bastante</h1>
+    <>
+      <h1>Base 2 by Web 4B Bueno, Bonito, Barato y Bastante</h1>
+      <ProductsCard />
+    </>
   );
 }
 
